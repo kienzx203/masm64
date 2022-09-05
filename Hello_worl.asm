ad61aa7ec5c0c154b2c4e5d3d6d6095a82f6e6b8
kinzx203 <97662987+kienzx203@users.noreply.github.com>
2022-09-05 18:50:42 +0700

    Update Hello_worl.asm

--- Hello_worl.asm
+++ Hello_worl.asm
@@ -3,27 +3,27 @@ extrn WriteFile		:PROC
 extrn ExitProcess	:PROC
 
 .data
-	msg		db "Hello_World", 0Ah, 0h
+	msg	db "Hello_World", 0Ah, 0h
 
 .data?
 	nByte	dd ?
 
 .code 
 main PROC
-	mov		rbp, rsp
-	sub		rsp, 28h
+	mov	rbp, rsp
+	sub	rsp, 28h
 
-	mov		rcx, -11			;shadow space + align
+	mov	rcx, -11		;shadow space + align
 	call	GetStdHandle		;return handle rax
-	xor		rbx, rbx
-	mov		rcx, rax			;Std_Output_Handle
-	mov		rdx, offset msg		
+	xor	rbx, rbx
+	mov	rcx, rax		;Std_Output_Handle
+	mov	rdx, offset msg		
 	mov     r8, sizeof msg		;lenght str
-	mov		r9, offset nByte	;return value agrument
-	mov		[rsp + 20h], rbx	
+	mov	r9, offset nByte	;return value agrument
+	mov	[rsp + 20h], rbx	
 	call	WriteFile
 
-	mov		rcx, 0
+	mov	rcx, 0
 	call	ExitProcess
 
 main ENDP
