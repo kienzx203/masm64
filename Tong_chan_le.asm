cc3111732210a26fd0b69f6369c93531f69aa531
kinzx203 <97662987+kienzx203@users.noreply.github.com>
2022-11-01 13:24:49 +0700

    Update Tong_chan_le.asm

--- Tong_chan_le.asm
+++ Tong_chan_le.asm
@@ -6,10 +6,11 @@ extrn ExitProcess	:PROC
 .data
 	Tong_chan	dq	0
 	Tong_le		dq	0
-	space		db  20h, 0
+	space		db  	20h, 0
 .data?
 	num_array	db	40 DUP (?)
 	num_output	db	40 DUP (?)
+	num		db	40 DUP (?)
 	len		dq	?
 	nByte		dd	?
 
@@ -20,7 +21,9 @@ main PROC
  	xor	rbx, rbx
 	mov	rcx, -10
 	call	GetStdHandle
+	mov	[rbp + 8], rax
 
+	xor	rbx, rbx
 	mov	rcx, rax
 	mov	rdx, offset num_array
 	mov	r8, 30
@@ -33,81 +36,125 @@ main PROC
 	call	ATOI
 	mov	len, rax
 
-L1:	
+	L1:
+		xor	rbx, rbx
+		mov	rcx, [rbp + 8]
+		mov	rdx, offset num_array
+		mov	r8, 30
+		mov	r9, offset nByte
+		mov	[rsp+20h], rbx
+		call	ReadFile
 
-	cmp	len, 0
-	jz	L4
-	xor	rbx, rbx
-	mov	rcx, -10
-	call	GetStdHandle
+		mov	rsi, offset num_array
+		mov	rdi, offset num
+	L2:
+		cmp	byte ptr [rsi], 0Dh
+		je	L4
+		cmp	byte ptr [rsi],20h
+		je	L3
+		mov	rax, [rsi]
+		mov	[rdi], rax
+		inc	rdi
+		inc	rsi
+		jmp	L2
+	L3:
+		mov	byte ptr [rdi], 0Dh
+		mov	rbx, rsi
+		mov	r12, offset num
+		push	r12
+		call	ATOI
+		mov	rsi, rbx
+		call	Sum
+		dec	len
+		inc	rsi
+		mov	rdi, offset num
+		jmp	L2
+	L4 :
+		mov	byte ptr[rdi], 0Dh
+		mov	rbx, rsi
+		mov	r12, offset num
+		push	r12
+		call	ATOI
+		mov	rsi, rbx
+		call	Sum
+		dec	len
+		cmp	len, 0
+		je	L5
+		jmp	L1
 
-	mov	rcx, rax
-	mov	rdx, offset num_array
-	mov	r8, 30
-	mov	r9, offset nByte
-	mov	[rsp+20h], rbx
-	call	ReadFile
-	dec	len
-	mov	r12, offset num_array
-	push	r12
-	call	ATOI
-	mov	rbx, 0
-	mov	rcx, 0
-	mov	rcx,rax
-	mov	rbx, 2
-	mov	rdx, 0
-	div	rbx
-	cmp	rdx, 0
-	jz	L3
-	jmp	L2
+	L5 :
+		mov	r12, Tong_chan
+		mov	r13, offset num_output
+		push	r12
+		push	r13
+		call	REATOI
+		xor	rbx, rbx
+		mov	rcx, -11
+		call	GetStdHandle
+		mov	rcx, rax
+		mov	rdx, offset num_output
+		mov	r8, 30
+		mov	r9, offset nByte
+		mov	[rsp + 20h], rbx
+		call	WriteFile
 
-L2:	
-	add	Tong_le, rcx
-	jmp	L1
-	
-L3: 
+		mov	r12, Tong_le
+		push	r12
+		mov	r13, offset num_output
+		push	r13
+		call	REATOI
 
-	add	Tong_chan, rcx
-	jmp	L1
+		xor 	rbx, rbx
+		mov	rcx, -11
+		call	GetStdHandle
+		mov	rcx, rax
+		mov	rdx, offset space
+		mov	r8, 2
+		mov	r9, offset nByte
+		mov	[rsp + 20h], rbx
+		call	WriteFile
 
-L4:
- 	mov	r12, Tong_chan
-	mov	r13, offset num_output
-	push	r12
-	push	r13
-	call	REATOI
-	xor	rbx, rbx
-	mov	rcx, -11
-	call	GetStdHandle
-	mov	rcx, rax
-	mov	rdx, offset num_output
-	mov	r8, 30
-	mov	r9, offset nByte
-	mov	[rsp+20h], rbx
-	call	WriteFile
+		xor	rbx, rbx
+		mov	rcx, -11
+		call	GetStdHandle
+		mov	rcx, rax
+		mov	rdx, offset num_output
+		mov	r8, 30
+		mov	r9, offset nByte
+		mov	[rsp + 20h], rbx
+		call	WriteFile
 
-	mov	r12, Tong_le
-	push	r12
-	mov	r13, offset num_output
-	push	r13
-	call	REATOI
-
-	xor	rbx, rbx
-	mov	rcx, -11
-	call	GetStdHandle
-	mov	rcx, rax
-	mov	rdx, offset num_output
-	mov	r8, 30
-	mov	r9, offset nByte
-	mov	[rsp+20h], rbx
-	call	WriteFile
-
-	mov	ecx, 0
-	call	ExitProcess
-
-
+		mov	rcx, 0
+		call	ExitProcess
 	
 main ENDP
+Sum PROC
+	push	rbp
+	mov	rbp, rsp
+	L1:
+		mov	rbx, 0
+		mov	rcx, 0
+		mov	rcx, rax
+		mov	rbx, 2
+		mov	rdx, 0
+		div	rbx
+		cmp	rdx, 0
+		jz	L3
+		jmp	L2
+
+	L2 :
+		add	Tong_le, rcx
+		pop	rbp
+		ret
+
+	L3 :
+
+		add	Tong_chan, rcx
+		pop	rbp
+		ret
+
+Sum ENDP
+
 ATOI PROC
 	push	rbp
 	mov	rbp, rsp
@@ -139,8 +186,8 @@ REATOI PROC
 
     push    rbp
     mov     rbp, rsp
-	xor		rax, rax
-	xor		rbx, rbx
+    xor		rax, rax
+    xor		rbx, rbx
     mov     rax, [rbp + 24]						
     mov     rbx, [rbp + 16]						
     xor     rsi, rsi 
@@ -166,7 +213,7 @@ L2:
     jmp     L2
 
 L3:
-    mov	    r8w, 0a0dh 
+    mov	    r8w, 0000h 
     mov     word ptr [rbx + rsi], r8w
     pop     rbp
     ret     16
